2205613c1c69954ed568c4d0a17ec79f5c2a390a
ThaysonScript <thaysonguedes01@gmail.com>
2023-10-25 16:26:17 -0300

    refactor - organizing script structure

--- rejuvenation/setup/remove-disks.sh
+++ rejuvenation/setup/remove-disks.sh
@@ -1,6 +1,14 @@
-UUIDS=`vboxmanage list hdds | sed -e '/./{H;$!d;}' -e 'x;/'"$GUEST"'/!d;'| grep UUID | egrep -v Parent| awk '{print $2}'`
-for u in $UUIDS
-do
- echo '--->> Deleteing: '$u
- vboxmanage closemedium disk $u --delete 
-done
+#!/usr/bin/env bash
+
+############################## VARS CONFIG
+UUIDS="$(vboxmanage list hdds | sed -e '/./{H;$!d;}' -e 'x;/'"$GUEST"'/!d;' | grep UUID | egrep -v Parent | awk '{print $2}')"
+
+############################## START FUNCTIONS
+REMOVING_DISKS() {
+    for u in $UUIDS; do
+        echo '--->> Deleteing: '"$u"
+        vboxmanage closemedium disk "$u" --delete
+    done
+}
+
+############################## END FUNCTIONS
