5ba3674a89e1b23e4b3657aabe95253708fe7687
ThaysonScript <thaysonguedes01@gmail.com>
2023-10-25 16:42:50 -0300

    refactor - organizing script structure

--- rejuvenation/setup/create-disks.sh
+++ rejuvenation/setup/create-disks.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-# Universidade Federal Rural de Pernambuco - Unidade Acadêmica de Garanhuns 
+
+################################################################################
+# Universidade Federal Rural de Pernambuco - Unidade Acadêmica de Garanhuns
 # Uname Research Group
 # Felipe Oliveira 09/11/2018
+# refactor - Thayson Guedes ( 25/10/2023 )
+################################################################################
 
-# Parameters 
-# $1 = number of disks
+################################### CONFIG VARS
+count=1           # counter control
+DISKS_QUANTITY=$1 # number of disks to be created
+DISK_SIZE=$2      # disk size after created
 
-count=1
+################################### START FUNCTIONS
+# create a new disks on virtual machine
+# parameters:
+#   $1 = $DISKS_QUANTITY
+#   $2 = $DISK_SIZE
+#
+# global variables:
+#   count
+CREATE_DISKS() {
+    while [ "$count" -lt "$DISKS_QUANTITY" ]; do
+        VBoxManage createmedium disk --filename disk$count.vhd --size "$DISK_SIZE" --format VHD --variant Fixed
 
-while [ $count -lt $1 ]
-do
-    VBoxManage createmedium disk --filename disk$count.vhd --size 1024 --format VHD --variant Fixed
-    count=`expr $count + 1`
-done
+        count=$(("$count" + 1))
+    done
+}
+################################### END FUNCTIONS
