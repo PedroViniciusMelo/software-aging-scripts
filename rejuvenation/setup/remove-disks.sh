7fee6e550106dcee81521750f5caa8fc043a1f6c
ThaysonScript <thaysonguedes01@gmail.com>
2023-10-26 10:47:34 -0300

    refactor - improving optimizing and handling errors when removing disks

--- rejuvenation/setup/remove-disks.sh
+++ rejuvenation/setup/remove-disks.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
-############################## VARS CONFIG
-UUIDS="$(vboxmanage list hdds | sed -e '/./{H;$!d;}' -e 'x;/'"$GUEST"'/!d;' | grep UUID | egrep -v Parent | awk '{print $2}')"
+############################## START VARS CONFIG
+# deprecated
+# UUIDS="$(vboxmanage list hdds | sed -e '/./{H;$!d;}' -e 'x;/'"$GUEST"'/!d;' | grep UUID | egrep -v Parent | awk '{print $2}')"
+
+# new
+UUIDS_DISKS="$(vboxmanage list hdds | grep -w "UUID:" | grep -v "Parent UUID:" | awk '{print $2}')" # get 'UUID' with 'id' and remove 'Parent UUID'
+############################## END VARS CONFIG
 
 ############################## START FUNCTIONS
 REMOVING_DISKS() {
-    for u in $UUIDS; do
-        echo '--->> Deleteing: '"$u"
-        vboxmanage closemedium disk "$u" --delete
+    for uuid_disk in $UUIDS_DISKS; do
+        echo -e "\n--->> Deleting disk with id: $uuid_disk \n"
+        deleting_disk="$(vboxmanage closemedium disk "$uuid_disk" --delete 2>&1)"
+
+        if [[ "$deleting_disk" == *"error:"* ]]; then
+            echo -e "Error: Failed to delete medium with UUID: $uuid_disk \n"
+            echo -e "***\nDetails: \n$deleting_disk \n***"
+        else
+            echo "Medium with UUID = $uuid_disk ( deleted successfully )"
+        fi
     done
 }
-
 ############################## END FUNCTIONS
+
+REMOVING_DISKS # using function
