119a063d9ff929c15e6420913153dd0c7468d489
ThaysonScript <thaysonguedes01@gmail.com>
2023-11-04 23:22:14 -0300

    fix errors

--- rejuvenation/setup/disk_management.sh
+++ rejuvenation/setup/disk_management.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 # Universidade Federal Rural de Pernambuco - Unidade Acadêmica de Garanhuns
 # Uname Research Group
 # Author - Thayson Guedes ( 31/10/2023 )
@@ -12,22 +11,6 @@ UUIDS_DISKS=$(vboxmanage list hdds | awk '/UUID:/ && !/Parent UUID:/ {print $2}'
 # CREATE_DISKS
 # DESCRIPTION:
 #   create a new disks on virtual machine
-=======
-################################################################################
-# Universidade Federal Rural de Pernambuco - Unidade Acadêmica de Garanhuns
-# Uname Research Group
-# Author - Thayson Guedes ( 31/10/2023 )
-################################################################################
-
-############################################################ START VARS CONFIG                                                                                
-COUNT=1                                                                                             # counter control
-UUIDS_DISKS="$(vboxmanage list hdds | grep -w "UUID:" | grep -v "Parent UUID:" | awk '{print $2}')" # get 'UUID' with 'id' and remove 'Parent UUID'
-############################################################ END VARS CONFIG
-
-############################################################ START FUNCTIONS
-# FUNCTION == CREATE_DISKS
-# DESCRIPTION == create a new disks on virtual machine
->>>>>>> d54b82f164400de4e96bc7a84b9549c8f1c4c627
 #
 # PARAMETERS:
 #   $1 = $DISKS_QUANTITY
@@ -44,7 +27,6 @@ CREATE_DISKS() {
     while [[ "$COUNT" -le "$DISKS_QUANTITY" ]]; do
         VBoxManage createmedium disk --filename ../disks/disk$COUNT.vhd --size "$DISK_SIZE" --format VHD --variant Fixed
 
-<<<<<<< HEAD
         ((COUNT++))
     done
 }
@@ -52,14 +34,6 @@ CREATE_DISKS() {
 # REMOVING_DISKS
 # DESCRIPTION:
 #   deletes virtual hard disks from the virtual machine except disks in use
-=======
-        COUNT=$((COUNT + 1))
-    done
-}
-
-# FUNCTION == REMOVING_DISKS
-# DESCRIPTION == deletes virtual hard disks from the virtual machine except disks in use
->>>>>>> d54b82f164400de4e96bc7a84b9549c8f1c4c627
 #
 # GLOBAL VARIABLES:
 #   $UUIDS_DISKS
@@ -80,7 +54,3 @@ REMOVING_DISKS() {
         fi
     done
 }
-<<<<<<< HEAD
-=======
-############################################################ END FUNCTIONS
->>>>>>> d54b82f164400de4e96bc7a84b9549c8f1c4c627
